4d24a610a3645f06fda63bbfc95f9207f3d0a84c
exalexi <exalexi@proton.me>
2025-03-09 16:54:30 +0100

    fixing zsh

--- home/home-apps/zsh.nix
+++ home/home-apps/zsh.nix
@@ -1,11 +1,36 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     package = pkgs.zsh;
+    enableCompletion = true;
+    autosuggestion = {
+      enable = true;
+      strategy = [
+        "history"
+        "completion"
+      ];
+    };
+    syntaxHighlighting.enable = true;
+    enableVteIntegration = true;
+
+    history = {
+      size = 10000;
+      ignoreAllDups = true;
+      path = "$HOME/.zsh_history";
+      ignorePatterns = [
+        "rm *"
+        "pkill *"
+        "cp *"
+        "sudo nixos-rebuild *"
+        "ls *"
+      ];
+    };
 
     shellAliases = {
-      update = "sudo rm /home/lex/.config/VSCodium/User/settings.json && sudo nixos-rebuild switch";
+      update = "sudo nixos-rebuild switch";
+      updateboot = "sudo nixos-rebuild boot";
+      vsconfig = "sudo rm /home/lex/.config/VSCodium/User/settings.json";
       ".." = "cd ..";
       "😼" = ''echo "nyaaaa :3"'';
       "😿" = ''echo "meowww 3:"'';
@@ -19,7 +44,11 @@
     initExtra = ''
       neofetch --distro_shorthand on --os_arch off --kernel_shorthand off --cpu_cores logical --cpu_temp C --gpu_type all --gtk_shorthand on --colors 225 219 231 225 225 189 --bold off --ascii_colors 225 117 0 0 0 0
     '';
+
   };
 
-  #programs.fzf.enable = true;
+  catppuccin.zsh-syntax-highlighting = {
+    enable = true;
+    flavor = "mocha";
+  };
 }
