1417662fdbad4fe6f36277dfcc3d8f724d10d2e5
exalexi <exalexi@proton.me>
2025-05-07 21:33:39 +0200

    remove nm-applet on boot service

--- config/system/nm-applet.nix
+++ config/system/nm-applet.nix
@@ -2,10 +2,10 @@
 {
   programs.nm-applet.enable = true;
 
-  systemd.user.services.nm-applet-on-boot = {
-    description = "Run nm-applet on boot";
-    serviceConfig.PassEnvironment = "DISPLAY";
-    script = "exec nm-applet";
-    wantedBy = [ "multi-user.target" ]; # starts after login
-  };
+  #systemd.user.services.nm-applet-on-boot = {
+  #  description = "Run nm-applet on boot";
+  #  serviceConfig.PassEnvironment = "DISPLAY";
+  #  script = "exec nm-applet";
+  #  wantedBy = [ "multi-user.target" ]; # starts after login
+  #};
 }
