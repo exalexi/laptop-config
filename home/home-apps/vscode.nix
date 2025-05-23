d89a043b7ec8c055746d767dee14a55431adf9da
exalexi <exalexi@proton.me>
2025-05-23 19:18:35 +0200

    vscode settings.json

--- home/home-apps/vscode.nix
+++ home/home-apps/vscode.nix
@@ -3,16 +3,30 @@
   programs.vscode = {
     enable = true;
     #package = pkgs.vscodium-fhs;
+    package = pkgs.vscode-fhs;
     extensions = with pkgs.vscode-extensions; [
       catppuccin.catppuccin-vsc-icons
       catppuccin.catppuccin-vsc
       jnoortheen.nix-ide
       redhat.java
+      vscjava.vscode-gradle
+      vscjava.vscode-java-debug
       ms-vscode-remote.remote-ssh
+      myriad-dreamin.tinymist
+      tomoki1207.pdf
+      raspberry-pi.raspberry-pi-pico
     ];
     userSettings = {
-
+      "[typst]" = {
+        "editor.wordSeperators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
+      };
+      "editor.formatOnSave" = true;
+      "files.autoSave" = "onFocusChange";
+      "git.enableSmartCommit" = true;
+      "java.jdt.ls.java.home" = "${pkgs.jdk21}";
       "nix.enableLanguageServer" = true;
+      "nix.formatterPath" = "nixfmt";
+      "nix.serverPath" = "nil";
       "nix.serverSettings" = {
         "nil" = {
           "formatting" = {
@@ -22,13 +36,10 @@
           };
         };
       };
-      "workbench.colorTheme" = "Catppuccin Mocha";
-      "files.autoSave" = "onFocusChange";
-      "editor.formatOnSave" = true;
-      "nix.formatterPath" = "nixfmt";
-      "nix.serverPath" = "nil";
-      "git.enableSmartCommit" = "true";
+      "redhat.telemetry.enabled" = false;
       "window.menuBarVisibility" = "toggle";
+      "workbench.colorTheme" = "Catppuccin Mocha";
+
     };
   };
 }
