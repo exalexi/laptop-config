7fbbb9e7ab20067e7c2e8e8ca5328f1777398ae5
exalexi <exalexi@proton.me>
2025-08-03 17:29:56 +0200

    fix config

--- home/home-apps/firefox.nix
+++ home/home-apps/firefox.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }:
 {
 
-  # Thanks for like 80% of this Leela 😼
   programs.firefox = {
     enable = true;
-
+    
     policies = {
       DefaultDownloadDirectory = "/home/lex/download";
       DisableFeedbackCommands = true;
@@ -25,49 +24,13 @@
       PasswordManagerEnabled = true;
       StartDownloadsInTempDirectory = true;
     };
-
+    /*
     profiles.default = {
       id = 0;
       name = "Default";
-      settings = {
-        "browser.startup.homepage" = "https://start.duckduckgo.com/?q=";
-        # "dom.security.https_only_mode" = true;
-        # "dom.security.https_only_mode_ever_enabled" = true;
-        # Privacy settings
-        "privacy.donottrackheader.enabled" = true;
-        "privacy.trackingprotection.enabled" = true;
-        "privacy.trackingprotection.socialtracking.enabled" = true;
-        "privacy.partition.network_state.ocsp_cache" = true;
-        # Disable all sorts of telemetry
-        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-        "browser.newtabpage.activity-stream.telemetry" = false;
-        "browser.ping-centre.telemetry" = false;
-        "toolkit.telemetry.archive.enabled" = false;
-        "toolkit.telemetry.bhrPing.enabled" = false;
-        "toolkit.telemetry.enabled" = false;
-        "toolkit.telemetry.firstShutdownPing.enabled" = false;
-        "toolkit.telemetry.hybridContent.enabled" = false;
-        "toolkit.telemetry.newProfilePing.enabled" = false;
-        "toolkit.telemetry.reportingpolicy.firstRun" = false;
-        "toolkit.telemetry.shutdownPingSender.enabled" = false;
-        "toolkit.telemetry.unified" = false;
-        "toolkit.telemetry.updatePing.enabled" = false;
-        # As well as Firefox 'experiments'
-        "experiments.activeExperiment" = false;
-        "experiments.enabled" = false;
-        "experiments.supported" = false;
-        "network.allow-experiments" = false;
-        # Disable Pocket Integration
-        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
-        "extensions.pocket.enabled" = false;
-        "extensions.pocket.api" = "";
-        "extensions.pocket.oAuthConsumerKey" = "";
-        "extensions.pocket.showHome" = false;
-        "extensions.pocket.site" = "";
-      };
       search = {
-        default = "ddg";
-        privateDefault = "ddg";
+        default = "duckduckgo";
+        privateDefault = "duckduckgo";
         force = true;
         engines = {
           "google".metaData.hidden = true;
@@ -133,6 +96,6 @@
         };
       };
       extensions.force = true;
-    };
+    };*/
   };
 }
--- home/home-apps/vscode.nix
+++ home/home-apps/vscode.nix
@@ -4,65 +4,55 @@
     enable = true;
     #package = pkgs.vscodium-fhs;
     package = pkgs.vscode-fhs;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      catppuccin.catppuccin-vsc-icons
-      catppuccin.catppuccin-vsc
-      jnoortheen.nix-ide
-      #redhat.java
-      #vscjava.vscode-gradle
-      #vscjava.vscode-java-debug
-      ms-vscode-remote.remote-ssh
-      #myriad-dreamin.tinymist
-      tomoki1207.pdf
-      ms-python.python
-
-      # QMK
-      editorconfig.editorconfig
-
-      xaver.clang-format
-      llvm-vs-code-extensions.vscode-clangd
-      bierner.github-markdown-preview
-
-      # Fuck you 😾
-      #raspberry-pi.raspberry-pi-pico
-    ];
-    profiles.default.userSettings = {
-      #"[typst]" = {
-      #  "editor.wordSeperators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
-      #};
-
-      #"editor.formatOnSave" = true;
-      "files.autoSave" = "onFocusChange";
-      "git.enableSmartCommit" = true;
-      #"java.jdt.ls.java.home" = "${pkgs.jdk21}";
-      #"nix.enableLanguageServer" = true;
-      #"nix.formatterPath" = "nixfmt";
-      #"nix.serverPath" = "nil";
-      #"nix.serverSettings" = {
-      #  "nil" = {
-      #    "formatting" = {
-      #      "command" = [
-      #        "nixfmt"
-      #      ];
-      #    };
-      #  };
-      #};
-      #"redhat.telemetry.enabled" = false;
-
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nixd";
-      "nix.serverSettings" = {
-        "nixd" = {
-          "formatting" = {
-            "command" = [ "nixfmt" ];
-          };
-        };
-      };
-
-      "nix.formatterPath" = "nixfmt";
-      "window.menuBarVisibility" = "toggle";
-      "workbench.colorTheme" = "Catppuccin Mocha";
-      "json.format.enable" = false;
-    };
+   # profiles.default.extensions = with pkgs.vscode-extensions; [
+   #   catppuccin.catppuccin-vsc-icons
+   #   catppuccin.catppuccin-vsc
+   #   jnoortheen.nix-ide
+   #   #redhat.java
+   #   #vscjava.vscode-gradle
+   #   #vscjava.vscode-java-debug
+   #   ms-vscode-remote.remote-ssh
+   #   #myriad-dreamin.tinymist
+   #   tomoki1207.pdf
+   #   ms-python.python
+   # ];
+  #  profiles.default.userSettings = {
+  #    #"[typst]" = {
+  #    #  "editor.wordSeperators" = "`~!@#$%^&*()=+[{]}\\|;:'\",.<>/?";
+  #    #};
+#
+  #    #"editor.formatOnSave" = true;
+  #    "files.autoSave" = "onFocusChange";
+  #    "git.enableSmartCommit" = true;
+  #    #"java.jdt.ls.java.home" = "${pkgs.jdk21}";
+  #    #"nix.enableLanguageServer" = true;
+  #    #"nix.formatterPath" = "nixfmt";
+  #    #"nix.serverPath" = "nil";
+  #    #"nix.serverSettings" = {
+  #    #  "nil" = {
+  #    #    "formatting" = {
+  #    #      "command" = [
+  #    #        "nixfmt"
+  #    #      ];
+  #    #    };
+  #    #  };
+  #    #};
+  #    #"redhat.telemetry.enabled" = false;
+#
+  #    "nix.enableLanguageServer" = true;
+  #    "nix.serverPath" = "nixd";
+  #    "nix.serverSettings" = {
+  #      "nixd" = {
+  #        "formatting" = {
+  #          "command" = [ "nixfmt" ];
+  #        };
+  #      };
+  #    };
+#
+  #    "nix.formatterPath" = "nixfmt";
+  #    "window.menuBarVisibility" = "toggle";
+  #    "workbench.colorTheme" = "Catppuccin Mocha";
+  #    "json.format.enable" = false;
+  #  };
   };
 }
